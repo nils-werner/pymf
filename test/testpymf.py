1a7d46adc8ec9da76a25320c1cb9511782c915eb
cthurau <cthurau@62694e12-420b-88a7-6f2b-4f1df9e58360>
2010-10-05 13:59:48 +0000

    added more tests (laesa, nmfnnls, nmfals,cmeans)
    git-svn-id: http://pymf.googlecode.com/svn/trunk@18 62694e12-420b-88a7-6f2b-4f1df9e58360

--- test/testpymf.py
+++ test/testpymf.py
@@ -32,18 +32,18 @@ def test_svd(A, func, desc, marker):
 	stime = time.time()
 	m = func(A, show_progress=False, rrank=2, crank=2)	
 	m.factorize()
-	print desc, m.frobenius_norm()/(A.shape[0] + A.shape[1]) , ' elapsed:' , time.time() - stime
+	print desc + ': Fro.:', m.frobenius_norm()/(A.shape[0] + A.shape[1]) , ' elapsed:' , time.time() - stime
 	return m
 
 
-def test(A, func, desc, marker):
+def test(A, func, desc, marker, niter=200):
 	stime = time.time()
 	
-	m = func(A, compW=True, num_bases=4, niter=200, show_progress=False)
+	m = func(A, compW=True, num_bases=4, niter=niter, show_progress=False)
 	m.initialization()	
 	m.factorize()
 	
-	print desc, m.ferr[-1]/(A.shape[0] + A.shape[1]) , ' elapsed:' , time.time() - stime
+	print desc + ': Fro.:', m.ferr[-1]/(A.shape[0] + A.shape[1]) , ' elapsed:' , time.time() - stime
 	return m
 
 def testsub(A, func, mfmethod, nsub, desc, marker):
@@ -63,20 +63,24 @@ B = scipy.sparse.csc_matrix(A)
 # test pseudoinverse
 pymf.pinv(A)
 pymf.pinv(B)
-svdm = test_svd(A, pymf.SVD, 'svd', 'c<')
+svdm = test_svd(A, pymf.SVD, 'Singula Value Decomposition (SVD)', 'c<')
 #svdm = test_svd(B, pymf.SVD, 'svd sparse', 'c<')
-curm = test_svd(A, pymf.CUR, 'cur', 'b<')
-curm = test_svd(B, pymf.CUR, 'cur sparse', 'b<')
-cmdm = test_svd(A, pymf.CMD, 'cmd', 'm<')
-cmdm = test_svd(B, pymf.CMD, 'cmd sparse', 'm<')
-sparse_svmcur = test_svd(A, pymf.SIVMCUR, 'sivmcur', 'm<')
-sparse_svmcur = test_svd(B, pymf.SIVMCUR, 'sivmcur sparse', 'm<')
-m = test(A, pymf.PCA, 'pca', 'c<')
-m = test(A, pymf.NMF, 'nmf', 'rs')
-m = test(A, pymf.SIVM, 'sivm', 'bs')
-m = test(A, pymf.Kmeans, 'kmeans', 'b*')
-m = test(A, pymf.AA, 'AA', 'bs')
-m = test(A, pymf.SNMF, 'snmf', 'bo')
-m = test(A, pymf.CHNMF, 'chnmf', 'm*')
-m = test(A, pymf.CNMF, 'cnmf', 'c<')
-m = test(A, pymf.BNMF, 'bnmf', 'b>')
+curm = test_svd(A, pymf.CUR, 'CUR Matrix Decomposition', 'b<')
+curm = test_svd(B, pymf.CUR, 'CUR Matrix Decomposition (sparse data)', 'b<')
+cmdm = test_svd(A, pymf.CMD, 'Compact Matrix Decomposition (CMD)', 'm<')
+cmdm = test_svd(B, pymf.CMD, 'Compact Matrix Decomposition (CMD - sparse data)', 'm<')
+sparse_svmcur = test_svd(A, pymf.SIVMCUR, 'Simplex Volume Maximization f. CUR (SIVMCUR)', 'm<')
+sparse_svmcur = test_svd(B, pymf.SIVMCUR, 'Simplex Volume Maximization f. CUR (SIVMCUR - sparse data)', 'm<')
+m = test(A, pymf.PCA, 'Principal Component Analysis (PCA)', 'c<')
+m = test(A, pymf.NMF, 'Non-negative Matrix Factorization (NMF)', 'rs')
+m = test(A, pymf.NMFALS, 'NMF u. alternating least squares (NMFALS)', 'rs', niter=10)
+m = test(A, pymf.NMFNNLS, 'NMF u. non-neg. least squares (NMFNNLS)', 'rs', niter=10)
+m = test(A, pymf.LAESA, 'Linear Approximating Eliminating Search Algorithm (LAESA)', 'rs')
+m = test(A, pymf.SIVM, 'Simplex Volume Maximization (SIVM)', 'bs')
+m = test(A, pymf.Kmeans, 'K-means clustering (Kmeans)', 'b*')
+m = test(A, pymf.Cmeans, 'C-means clustering (Cmeans)', 'b*')
+m = test(A, pymf.AA, 'Archetypal Analysis (AA)', 'bs')
+m = test(A, pymf.SNMF, 'Semi Non-negative Matrix Factorization (SNMF)', 'bo')
+m = test(A, pymf.CNMF, 'Convex non-negative Matrix Factorization (CNMF)', 'c<')
+m = test(A, pymf.CHNMF, 'Convex-hull non-negative Matrix Factorization (CHNMF)', 'm*')
+m = test(np.round(A-2.0), pymf.BNMF, 'Binary Matrix Factorization (BNMF)', 'b>')
