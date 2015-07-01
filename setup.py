f0a0826b6a8b19ae71d4f0917fbf2b6e23a495cd
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-07-01 11:15:05 +0200

    Fixed setuptools implementation
    Numpy and Scipy were missing from dependencies and it was not installing correctly using `pip install -e .`

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-import os
-from setuptools import setup
+import setuptools
 
-setup(name='PyMF',
-      version='0.1.9',
-      description='Python Matrix Factorization Module',
-      author='Christian Thurau',
-      author_email='cthurau@googlemail.com',
-      url='http://code.google.com/p/pymf/',
-      packages = ['pymf'],    
-      package_dir = {'pymf': './lib/pymf'},   
-      scripts=['scripts/testpymf.py',],      
-      license = 'OSI Approved :: GNU General Public License (GPL)',
-      install_requires=['cvxopt'],
-      long_description=open('README.txt').read(),
-      )     
+setuptools.setup(
+    name='PyMF',
+    version='0.1.9',
+    description='Python Matrix Factorization Module',
+    author='Christian Thurau',
+    author_email='cthurau@googlemail.com',
+    url='http://code.google.com/p/pymf/',
+    packages=setuptools.find_packages(),
+    license='OSI Approved :: GNU General Public License (GPL)',
+    install_requires=[
+        'cvxopt',
+        'numpy',
+        'scipy',
+    ],
+    long_description=open('README.txt').read(),
+)
